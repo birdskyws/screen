53ee6deaf87f401f279726b62a38b2cb82615d3f
wangsen <wangsen@jfjb.com.cn>
2016-04-08 01:01:16 +0800

    add init bat

---
+++ exe/init.bat
@@ -0,0 +1,2 @@
+cp d:\xampp\htdocs\jfjb\exe\start.bat d:\xampp\htdocs\app.bat 
+start d:\xampp\htdocs\app.bat 
--- exe/start.bat
+++ exe/start.bat
@@ -1,2 +1,7 @@
-start d:\xampp\htdocs\jfjb\app.exe
-start d:\xampp\htdocs\jfjb\timer.exe
+cp d:\xampp\htdocs\jfjb\exe\app.exe d:\xampp\htdocs\app.exe 
+cp d:\xampp\htdocs\jfjb\exe\timer.exe d:\xampp\htdocs\timer.exe 
+start d:\xampp\htdocs\app.exe
+start d:\xampp\htdocs\timer.exe 
+start d:\xampp\htdocs\jfjb\exe\sleep.exe /enable
+cd d:\xampp\htdocs\jfjb
+"C:\Program Files\Git\bin\git.exe" pull
