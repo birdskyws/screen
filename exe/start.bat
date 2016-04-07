452f37b7ed1254aad3de8587dedcb755caaf1bd6
wangsen <wangsen@jfjb.com.cn>
2016-04-08 01:08:30 +0800

    modify start.bat

--- exe/start.bat
+++ exe/start.bat
@@ -1,5 +1,5 @@
-cp d:\xampp\htdocs\jfjb\exe\app.exe d:\xampp\htdocs\app.exe 
-cp d:\xampp\htdocs\jfjb\exe\timer.exe d:\xampp\htdocs\timer.exe 
+copy d:\xampp\htdocs\jfjb\exe\app.exe d:\xampp\htdocs\app.exe 
+copy d:\xampp\htdocs\jfjb\exe\timer.exe d:\xampp\htdocs\timer.exe 
 start d:\xampp\htdocs\app.exe
 start d:\xampp\htdocs\timer.exe 
 start d:\xampp\htdocs\jfjb\exe\sleep.exe /enable
