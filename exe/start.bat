f25c23f84cf5ef0358ad86e88ad8d3b04da00cfd
wangsen <wangsen@jfjb.com.cn>
2016-04-11 17:05:53 +0800

    修改了start.bat

--- exe/start.bat
+++ exe/start.bat
@@ -3,5 +3,6 @@ copy d:\xampp\htdocs\jfjb\exe\timer.exe d:\xampp\htdocs\timer.exe
 start d:\xampp\htdocs\app.exe
 start d:\xampp\htdocs\timer.exe 
 start d:\xampp\htdocs\jfjb\exe\sleep.exe /enable
+d:
 cd d:\xampp\htdocs\jfjb
 "C:\Program Files\Git\bin\git.exe" pull
